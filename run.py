571a9a33ff096eb6bbb36a309a2e943673067318
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2023-11-25 21:50:44 +0100

    create get_sales_data function to collect sales data string from user

--- run.py
+++ run.py
@@ -14,8 +14,23 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+"""
+Code used to check Google sheets API is working
+
 sales = SHEET.worksheet('sales') #to access 'sales' worksheet inside 'love-sandwishes' spreadsheet
-
 data = sales.get_all_values() #gets all values from sales worksheet
-
 print(data)
+"""
+
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
